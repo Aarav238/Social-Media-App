72dca811fce18fcdd2129c07eb02fec5c3e281bc
Aarav238 <aarav8090shukla@gmail.com>
2023-01-02 03:16:17 +0530

    mongodb setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-import  Express  from "express";
+import  express  from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
@@ -15,8 +15,8 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
-const app = Express();
-app.use(Express.json());
+const app = express();
+app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
 app.use(morgan("common"));
@@ -38,3 +38,14 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+
+
+//mongoose setup
+
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=> {
+    app.listen(PORT, () => console.log(`server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect `));
