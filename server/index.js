7696c4fcb4f6a1b375f911ee056197b775f3413a
Aarav238 <aarav8090shukla@gmail.com>
2023-01-02 02:51:03 +0530

    index.js

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import  Express  from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+//Configurations
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = Express();
+app.use(Express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("common"));
+app.subscribe(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+app.use("/assets",express.static(path.join(__dirname,'public/assets')));
+
+
+//file storage
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,"public/assets");
+    },
+    filename: function(req,file,cb){
+        cb(null,file.originalname)
+    }
+});
+
+const upload = multer({storage});
