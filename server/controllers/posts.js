c8c60d552acd35bcb0055971fe92bcd3ff5a7945
Aarav238 <aarav8090shukla@gmail.com>
2023-01-09 13:57:08 +0530

    update post setup

---
+++ server/controllers/posts.js
@@ -0,0 +1,47 @@
+
+import Post from "../models/Post"; 
+import User from "../models/User";
+
+
+//create
+
+
+export const createPost = async(req,res) => {
+    try {
+        const {userId, description , picturePath} = req.body;
+        const user = await User.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            picturePath,
+            likes: {},
+            comments: []
+        })
+
+
+        await newPost.save();
+
+        const post = await Post.find();
+        res.status(201).json(post);
+
+    } catch (err) {
+        res.status(409).json({message: err.message})
+        
+    }
+}
+
+
+
+//read
+
+export const getFeedPosts = async(req,res) => {
+    try {
+        
+    } catch (err) {
+        res.status(404).json({message: err.message})
+    }
+}
