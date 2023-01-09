f5e7b5f7e1397dc54ff20e1ed7deccff03369446
Aarav238 <aarav8090shukla@gmail.com>
2023-01-09 15:43:40 +0530

    adding data to database

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js"
 import {register} from "./controllers/auth.js";
 import {createPost} from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js"
+import Post from "./models/Post.js"
+import {users, posts} from "./data/index.js"
 
 
 //Configurations
@@ -66,4 +69,8 @@ mongoose.connect(process.env.MONGO_URL,{
     useUnifiedTopology: true,
 }).then(()=> {
     app.listen(PORT, () => console.log(`server Port: ${PORT}`));
+
+//add data one time
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error) => console.log(`${error} did not connect `));
