9b17b01bd6e0752148411b6c7762e1d9ce21faed
Aarav238 <aarav8090shukla@gmail.com>
2023-01-09 22:57:36 +0530

    configure theme setting

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,28 @@ import ProfilePage from "scenes/profilePage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
 import { themeSettings } from "./theme";
 
+
 function App() {
+
+  const mode = useSelector((state) => state.mode);
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className="app">
       <BrowserRouter>
+      <ThemeProvider theme={theme}>
+      <CssBaseline/>
         <Routes>
           <Route path="/" element={<LoginPage/>}/>
           <Route path="/home" element={<HomePage/>}/>
           <Route path="/profile/:userId" element={<ProfilePage/>}/>
 
         </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
