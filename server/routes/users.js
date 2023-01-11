ce4c75b07a4867b00e4d93eee6b518e9c9f1e503
Aarav238 <aarav8090shukla@gmail.com>
2023-01-11 21:33:35 +0530

    Error fixed

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,23 +1,18 @@
 import express from "express";
 import {
-    getUser,
-    getUserFriends,
-    addRemoveFriend,
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
-
 const router = express.Router();
 
-//Read
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
 
-router.get("/:id",verifyToken,getUser);
-
-
-router.get("/:id/friends",verifyToken,getUserFriends);
-
-//update
-router.patch("/:id/:friendId",verifyToken,addRemoveFriend);
-
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
