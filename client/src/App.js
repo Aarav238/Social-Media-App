5cadbb17c129d8f24a9d235af49b4c2b387df098
Aarav238 <aarav8090shukla@gmail.com>
2023-01-11 14:53:09 +0530

    login and register form complete

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
   const mode = useSelector((state) => state.mode);
 
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -22,8 +23,8 @@ function App() {
       <CssBaseline/>
         <Routes>
           <Route path="/" element={<LoginPage/>}/>
-          <Route path="/home" element={<HomePage/>}/>
-          <Route path="/profile/:userId" element={<ProfilePage/>}/>
+          <Route path="/home" element={isAuth ?<HomePage/> : <Navigate to="/"/>}/>
+          <Route path="/profile/:userId" element={isAuth ? <ProfilePage/> : <Navigate to="/"/>}/>
 
         </Routes>
         </ThemeProvider>
